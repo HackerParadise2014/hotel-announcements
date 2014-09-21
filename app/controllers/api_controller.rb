b355e09ab377593e842f22bba6d7dcf72449c5fa
ogoog <marc.gugliuzza@gmail.com>
2014-09-21 13:16:24 -0600

    Created a new branch for working on two-way features

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,26 +2,15 @@ class ApiController < ApplicationController
 
   def nexmo_receiver
 
-	puts params
+	# puts params
 
-	# require 'slack/post'
-	# Slack::Post.configure(
-	#   subdomain: 'hackerparadise2014',
-	#   token: 'PmeYLdrdKKyRiVy0B5ckQXum',
-	#   username: 'marc'
-	# )
- #  Slack::Post.post "@channel #{params[:text]}", '#hotel-announcements'
+	require 'slack-notifier'
 
+	notifier = Slack::Notifier.new "hackerparadise2014", "#{ENV['SLACK_TOKEN']}"
 
- require 'slack-notifier'
+	notifier.username = 'El Sueno Tropical'
 
-notifier = Slack::Notifier.new "hackerparadise2014", "#{ENV['SLACK_TOKEN']}"
-
-
-notifier.username = 'El Sueno Tropical'
-
-notifier.ping "<!channel> #{params[:text]}", channel: "#general", icon_url: 'http://cdn.graphicsfactory.com/clip-art/image_files/image/7/708587-dancing020yy.gif'
-
+	response = notifier.ping "<!channel> TESTING", channel: "#TESTING", icon_url: 'http://cdn.graphicsfactory.com/clip-art/image_files/image/7/708587-dancing020yy.gif'
 
 	render text: params
   end
